dd257c60a8b00de8b781d2e5695af0d480be8260
RamonMengarda <ramon_hiago@hotmail.com>
2022-07-22 21:48:26 -0300

    Add getPosts with pagination and sort

---
+++ src/main/java/com/ramonmengarda/posterr/controller/PostController.java
@@ -0,0 +1,34 @@
+package com.ramonmengarda.posterr.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramonmengarda.posterr.model.Post;
+import com.ramonmengarda.posterr.repository.PostRepository;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin
+public class PostController {
+    
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/page/{pageNumber}")
+    public Page<Post> getPosts(@PathVariable int pageNumber){
+        //CONSTRAINT: Load posts on chunks of 10 posts, ordered from latest to oldest
+        Pageable pageable = PageRequest.of(pageNumber, 10, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        return postRepository.findAll(pageable);
+    }
+}
